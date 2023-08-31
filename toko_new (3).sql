02d2435530eae3b1e2cdb10ca35b998f2d424d06
Abizar01 <98296855+Abizar01@users.noreply.github.com>
2023-08-31 14:39:53 +0700

    Add files via upload

---
+++ toko_new (3).sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 31, 2023 at 09:38 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+-- Error reading structure for table toko_new.detail_transaksi: #1932 - Table 'toko_new.detail_transaksi' doesn't exist in engine
+-- Error reading data for table toko_new.detail_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko_new`.`detail_transaksi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+-- Error reading structure for table toko_new.pelanggan: #1932 - Table 'toko_new.pelanggan' doesn't exist in engine
+-- Error reading data for table toko_new.pelanggan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko_new`.`pelanggan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+-- Error reading structure for table toko_new.petugas: #1932 - Table 'toko_new.petugas' doesn't exist in engine
+-- Error reading data for table toko_new.petugas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko_new`.`petugas`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+-- Error reading structure for table toko_new.produk: #1932 - Table 'toko_new.produk' doesn't exist in engine
+-- Error reading data for table toko_new.produk: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko_new`.`produk`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+-- Error reading structure for table toko_new.transaksi: #1932 - Table 'toko_new.transaksi' doesn't exist in engine
+-- Error reading data for table toko_new.transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `toko_new`.`transaksi`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
